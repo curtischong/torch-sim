1601d5a69dc027b595b59ab3ac5ed2e2f3887d23
Curtis Chong <curtischong5@gmail.com>
2025-11-02 08:28:55 -0800

    make simstate more lenient for cell shapes

--- torch_sim/state.py
+++ torch_sim/state.py
@@ -134,10 +134,10 @@ class SimState:
             if not torch.all(counts == torch.bincount(self.system_idx)):
                 raise ValueError("System indices must be unique consecutive integers")
 
-        if self.cell.ndim != 3 and self.system_idx is None:
+        if self.cell.ndim == 2:
             self.cell = self.cell.unsqueeze(0)
 
-        if self.cell.shape[-2:] != (3, 3):
+        if self.cell.ndim != 3 or self.cell.shape[-2:] != (3, 3):
             raise ValueError("Cell must have shape (n_systems, 3, 3)")
 
         if self.cell.shape[0] != self.n_systems:
